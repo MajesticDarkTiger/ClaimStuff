39bef8c5ae07407527f71ca8a7bf29417510e282
MajesticDarkTiger <darktiger533@gmail.com>
2023-03-03 10:53:57 -0600

    Added phonebook in homework

---
+++ PhoneBook/Account.java
@@ -0,0 +1,7 @@
+package homeWork.PhoneBook;
+
+public class Account {
+
+    
+    
+}
--- PhoneBook/Main.java
+++ PhoneBook/Main.java
@@ -1,11 +1,16 @@
 package homeWork.PhoneBook;
 
+import java.util.Scanner;
+
 public class Main {
+
     
+    static Scanner scr = new Scanner(System.in);
+
     public static void main(String[] args) {
         
 
-        
+
     }
 
 }
